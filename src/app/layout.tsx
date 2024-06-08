b2b7dc194852eaa2ea35bb532fa14871b33b86ca
Thushan W A R T <rusiththarindu2@gmail.com>
2024-06-08 21:34:22 +0530

    feat: Navigation Bar created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {Nunito} from "next/font/google"
+import Navbar from "../components/navbar/Navbar";
 
 const font = Nunito(
   {
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+        </body>
     </html>
   );
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import Container from "../Container"
+import Logo from "../navbar/Logo"
+import Search from "../navbar/Search"
+import UserMenu from "./UserMenu"
+
+const Navbar = () => {
+    return ( 
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+                        <Logo/>
+                        <Search/>
+                        <UserMenu/>
+                    </div>
+                </Container>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
+<div>
+    I am the Navbar
+</div>
