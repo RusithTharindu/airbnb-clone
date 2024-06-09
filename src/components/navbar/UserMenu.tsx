cb211dced098f611cd91e4aa36fe0288239afb31
Thushan W A R T <rusiththarindu2@gmail.com>
2024-06-10 01:24:33 +0530

    feat: MenuItems Added

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,9 +1,17 @@
-"use Client"
+"use client"
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "./Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen ((value) => !value);
+    }, []);
+
     return ( 
         <div className="relative ">
             <div className="flex flex-row items-center gap-3">
@@ -11,7 +19,7 @@ const UserMenu = () => {
                     {/*Need to give an onclick function later */}
                     Airbnb your Home
                 </div>
-                <div className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-100 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
+                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-100 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     {/*Need to give an onclick function later */}
                     <AiOutlineMenu/>
                     <div className="hidden md:block ">
@@ -19,6 +27,23 @@ const UserMenu = () => {
                     </div>
                 </div>
             </div>
+
+            {isOpen && (
+                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex- flex-col cursor-pointer">
+                        <>
+                            <MenuItem 
+                                onClick={() => {}}
+                                label="Login"
+                            />
+                            <MenuItem 
+                                onClick={() => {}}
+                                label="Sign Up"
+                            />
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
      );
 }
