a381095b533fb5a4e0746ee9722b73048a1a02a3
Thushan W A R T <rusiththarindu2@gmail.com>
2024-06-23 23:59:03 +0530

    feat: Modals pt.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          {/* <Modal/> */}
+          {/* <Modal isOpen title="hello World"/> */}
           <Navbar/>
         </ClientOnly>
         {children}
