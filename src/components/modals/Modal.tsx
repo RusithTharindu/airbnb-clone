bfc78aa3ff122fb0b04efb24d22aa04a91f4482a
Thushan W A R T <rusiththarindu2@gmail.com>
2024-06-27 23:39:50 +0530

    feat: Register modal completed

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -121,6 +121,7 @@ const Modal: React.FC<ModalProps> = ({
                                         onClick={handleSubmit}
                                     />
                                 </div>
+                                {footer}
                             </div>
                         </div>
                     </div>
--- src/components/modals/RegisterModal.tsx
+++ src/components/modals/RegisterModal.tsx
@@ -15,6 +15,7 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/input';
 import toast from 'react-hot-toast';
+import Button from '../Button'
 
 
 const RegisterModal = () => {
@@ -83,6 +84,37 @@ const RegisterModal = () => {
         </div>
     )
 
+    const footerContent = (
+        <div className='flex flex-col gap-4 mt-3'>
+            <hr />
+            <Button 
+                outline
+                label = "Continue with Google"
+                Icon = {FcGoogle}
+                onClick={() => {}}
+            />
+            <Button 
+                outline
+                label = "Continue with GitHub"
+                Icon = {AiFillGithub}
+                onClick={() => {}}
+            />
+            <div className='text-neutral-500 text-center mt-4 font-light'>
+                <div className='flex flex-row items-center justify-center gap-2'>
+                    <div>
+                        Already have an Account?
+                    </div>
+                    <div 
+                        className='text-neutral-800 cursor-pointer hover:underline ' 
+                        onClick={registerModal.onClose}
+                    >
+                        Log In
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
     return (
         <Modal
             disabled={isLoading}
@@ -92,6 +124,7 @@ const RegisterModal = () => {
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
         />
      );
 }
